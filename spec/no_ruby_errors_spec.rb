8276e031c72379ddb3385f7c2091c3ad40893e25
Virginia Dooley <vcdooley@gmail.com>
2018-09-18 15:47:09 +0000

    automatically backed up by learn

--- spec/no_ruby_errors_spec.rb
+++ spec/no_ruby_errors_spec.rb
@@ -1,6 +1,4 @@
-var NameError
-  
-  describe "Not having any errors and being all green" do
+describe "Not having any errors and being all green" do
 
   context 'NameError' do
     it 'raises a NameError when encountering undefined barewords' do
